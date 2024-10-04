0cebb32406579b323ca69b71ce417ad75d046a13
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-04 05:02:02 -0300

    feat: add button component and improve UI
    Add the `Button` component from Radix UI for better styling and accessibility, enabling a basic interactive element. Refactor global styles and improve the overall user interface.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   )
 }
