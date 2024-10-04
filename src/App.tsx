e5add54076f0b23a1eb8bc26e772ea1ff01ab44c
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-04 05:31:15 -0300

    feat: add prettier-plugin-tailwindcss
    Improves code formatting consistency by integrating prettier-plugin-tailwindcss for consistent Tailwind CSS styling. This ensures that styles are consistently applied and helps maintain code readability.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <Button>Enviar</Button>
     </div>
   )
