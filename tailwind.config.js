3d2cdd854af76af0553685b4f66796c52e489de3
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-04 05:04:21 -0300

    fix: Add Tailwind content paths
    Include all relevant project directories for Tailwind CSS to scan for classes. This ensures that Tailwind can properly process all styles within the application.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: [],
+    content: [
+			"./pages/**/*.{ts,tsx}",
+			"./components/**/*.{ts,tsx}",
+			"./app/**/*.{ts,tsx}",
+			"./src/**/*.{ts,tsx}",
+		],
   theme: {
   	extend: {
   		borderRadius: {
