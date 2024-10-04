8979e42e3d9beed831021d6970cee97fa84c7485
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-04 06:02:36 -0300

    feat: Add Helmet for SEO
    Adds `react-helmet-async` for managing meta tags and title tags, improving SEO and site structure.
    
    Includes basic Helmet usage in the `App` component and layout components for better title management.

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,13 +1,15 @@
+import { Helmet } from 'react-helmet-async'
 import { Outlet } from 'react-router-dom'
 
 export function AppLayout() {
   return (
-    <div>
+    <>
+      <Helmet title="Dashboard" />
       <h1>Header</h1>
 
       <div>
         <Outlet />
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,15 @@
+import { Helmet } from 'react-helmet-async'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
+    <>
+      <Helmet title="Login" />
       <h1>Autenticação</h1>
 
       <div>
         <Outlet />
       </div>
-    </div>
+    </>
   )
 }
