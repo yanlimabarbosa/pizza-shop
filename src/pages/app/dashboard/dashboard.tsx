0829e257e325b0149075289ca42c32c39d42ca49
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-06 19:43:33 -0300

    feat: add revenue chart to dashboard
    Adds a revenue chart to the dashboard to provide a visual representation of revenue trends. This chart will help users gain insights into revenue performance over time.

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrderAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
 import { MonthOrderAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
   return (
@@ -17,6 +18,10 @@ export function Dashboard() {
           <MonthOrderAmountCard />
           <DayOrderAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
