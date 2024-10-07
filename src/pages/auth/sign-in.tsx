231be1660f8ca4303542ed6102c389724e32afe9
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-07 17:39:37 -0300

    feat: Implement restaurant registration and sign-in flow
    Adds the ability for users to register their restaurants and sign in using the provided email address. This includes:
    
    - Implementing the `registerRestaurant` API call.
    - Storing the user's email in the sign-in form for convenience.
    - Providing a success message with a link to the sign-in page.
    
    This feature enhances the user experience by allowing restaurants to easily register and begin using the platform.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,7 +3,7 @@ import { Label } from '@radix-ui/react-label'
 import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -18,12 +18,15 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<SignInForm>({
     resolver: zodResolver(signInForm),
+    defaultValues: { email: searchParams.get('email') ?? '' },
   })
 
   const { mutateAsync: authenticate } = useMutation({
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,11 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Label } from '@radix-ui/react-label'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
@@ -29,14 +31,22 @@ export function SignUp() {
     resolver: zodResolver(signUpForm),
   })
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignUp(data: SignUpForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-      console.log(data)
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
       toast.success('Restaurante cadastrado com sucesso!', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in'),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       })
     } catch (error) {
