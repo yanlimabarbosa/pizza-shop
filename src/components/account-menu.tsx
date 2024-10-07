27d90e6dcacc8784dc7958fb4795b523469378ea
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-07 17:53:07 -0300

    feat: Add authentication and user profile
    Adds basic authentication and user profile fetching. Includes:
    
    - Authentication via a user's session.
    - Fetching the user's name and email.
    - Fetching the name of the managed restaurant.
    - Displaying user information in the account menu.
    
    This enables users to log in and see their basic profile information in the application.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,4 +1,8 @@
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
 
 import { Button } from './ui/button'
 import {
@@ -11,6 +15,18 @@ import {
 } from './ui/dropdown-menu'
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
+  console.log(profile)
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,15 +34,15 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56 p-2">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Yan Barbosa</span>
+          <span>{profile?.name}</span>
           <span className="text-sm font-normal text-muted-foreground">
-            yanbr763@gmail.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
