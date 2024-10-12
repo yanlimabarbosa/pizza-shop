c1c64b7e914c1558a64861f8a9820b6c3d679449
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-12 17:28:27 -0300

    Fix: Add support for running unit tests
    This commit addresses the issue of missing test configuration by adding a test section to `vite.config.ts` and including test files in the TypeScript compiler's search path. This enables unit tests to be run correctly and provides a robust testing environment for the application.

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
-    enviroment: 'happy-dom',
+    environment: 'happy-dom',
   } as TestOptions,
 } as UserConfig)
 
