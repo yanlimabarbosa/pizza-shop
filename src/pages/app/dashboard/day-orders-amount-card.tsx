39d1bb8b8b539cdf3c2b3dd4866eff848e442a6c
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-12 06:36:24 -0300

    feat: Add loading state to dashboard cards
    Introduces a skeleton component to display loading indicators while fetching data for the dashboard cards. This enhances user experience by providing visual feedback and reducing perceived loading times.

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -4,6 +4,8 @@ import { Utensils } from 'lucide-react'
 import { getDayOrdersAmount } from '@/api/get-day-order-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
+import { MetricCardSkeleton } from './metric-card-skeleton'
+
 export function DayOrderAmountCard() {
   const { data: dayOrdersAmount } = useQuery({
     queryKey: ['metrics', 'day-orders-amount'],
@@ -17,7 +19,7 @@ export function DayOrderAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {dayOrdersAmount && (
+        {dayOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {dayOrdersAmount.amount.toLocaleString('pt-BR')}
@@ -35,6 +37,8 @@ export function DayOrderAmountCard() {
               em relação a ontem
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
