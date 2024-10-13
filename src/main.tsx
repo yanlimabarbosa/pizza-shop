5caaf17e4e20be2b8c23013ca4ebe375af6999c9
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-12 22:06:41 -0300

    feat: Implement MSW for API mocking
    Adds Mock Service Worker (MSW) for API mocking during development and testing, enhancing development efficiency and providing a controlled environment for testing. This implementation allows for seamless mocking of API requests, enabling faster development cycles and more reliable testing scenarios.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { enableMSW } from './api/mocks'
 import { App } from './app'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
