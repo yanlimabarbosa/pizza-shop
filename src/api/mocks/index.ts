337565aed8a724b3e0e078acbba6c10a0466161e
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-13 00:54:02 -0300

    feat: Add mocks for profile and restaurant management to index page
    Add mocks for the following API endpoints:
    
    - Get managed restaurant
    - Get profile
    - Update profile
    
    This provides a more complete set of mocks for testing components and features related to profile and restaurant management.

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -3,13 +3,16 @@ import { setupWorker } from 'msw/browser'
 import { env } from '@/env'
 
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount'
 import { getMonthRevenueMock } from './get-month-revenue'
 import { getPopularProductsMock } from './get-popular-products-mock'
+import { getProfileMock } from './get-profile-mock'
 import { getDayOrdersAmountMock } from './gey-day-orders-amount-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -20,6 +23,9 @@ export const worker = setupWorker(
   getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
   getPopularProductsMock,
+  getManagedRestaurantMock,
+  getProfileMock,
+  updateProfileMock,
 )
 
 export async function enableMSW() {
