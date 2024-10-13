97ca3d6919a0c388a99d7ef330c56ed918b40e3f
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-13 16:35:36 -0300

    test(e2e): add end-to-end tests for sign-in page

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './test',
+  testMatch: /.*\.e2e-spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
