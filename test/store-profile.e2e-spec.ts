7e97426b2d1693eec6f494999ad2d0b6ebbbcc19
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-14 20:47:04 -0300

    feat: add e2e test for restaurant profile update
    Adds an end-to-end test that verifies the store profile update functionality. This test covers filling in the update form, submitting it, and validating the success message and updated store name.

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByText('Perfil da Loja').click()
+
+  await page.getByLabel('Nome').fill('Best Pizza')
+  await page.getByLabel('Descrição').fill('Random Desc')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(250)
+
+  expect(page.getByRole('button', { name: 'Best Pizza' })).toBeVisible()
+})
